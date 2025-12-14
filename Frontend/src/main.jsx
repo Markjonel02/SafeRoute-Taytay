5c171e6c46b55fda01c7de4c038910d22563e8e5
Markjonel02 <rellesmarkjonel14@gmail.com>
2025-12-14 14:14:57 +0800

    oke with structure

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { ChakraProvider } from "@chakra-ui/react";
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <ChakraProvider>
+import { Provider } from "../src/components/ui/provider";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider>
       <App />
-    </ChakraProvider>
-  </StrictMode>
+    </Provider>
+  </React.StrictMode>
 );
